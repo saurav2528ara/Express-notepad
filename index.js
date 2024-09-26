b433b9ea608961e37dbf1f5e673f8d2435a2d823
saurav <saurav2528ara@gmail.com>
2024-09-26 23:54:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const host = 5000;
+const path = require('path');
+const fs = require('fs');
+const { log } = require('console');
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function(req, res, next){
+    console.log('middle ware');
+    next();
+});
+
+app.get('/', function(req, res){
+    fs.readdir(`./files`, function(err, files){
+        res.render("index", {files: files});
+    });
+});
+
+app.post('/create', function(req, res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/");
+   });
+    
+});
+
+app.get('/about/:username', function(req, res){
+    res.send(`Welcome, ${req.params.username}`);
+});
+
+app.get('/files/:filename', function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render("show", {filename: req.params.filename, filedata: filedata});
+    })
+});
+
+app.get('/edit/:filename', function(req, res){
+    res.render("edit", {filename: req.params.filename});
+});
+
+app.post('/edit', function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`,function(err){
+        res.redirect("/");
+    });
+});
+
+app.listen(host, () => {
+    console.log(`Server is running at ${host}`);
+});
